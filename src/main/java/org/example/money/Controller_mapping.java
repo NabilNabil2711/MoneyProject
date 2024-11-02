b2eef335e505608256679aee4e8f5ab67cfddf5b
NabilNabil2711 <nabil.nabil@otto.de>
2024-11-02 18:21:11 +0100

    ALBA-0001| Nabil | inputfield works and connection to java learned (#1)

---
+++ src/main/java/org/example/money/Controller_mapping.java
@@ -0,0 +1,39 @@
+package org.example.money;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+
+@Controller
+public class Controller_mapping {
+    MoneyApplication application=new MoneyApplication();
+
+
+    //Call website
+    //------------------------
+    @GetMapping("testHtml")
+    public String hello() {
+        return "index";
+    }
+    @GetMapping("calculate")
+    public String calculate() {
+        return "Calculator";
+    }
+
+    //Get input from User
+    //-------------------------
+    @PostMapping("/getSalary")
+    @ResponseBody
+    public String getSalary(@RequestParam("salary")int salary,@RequestParam("savingsrate")int savingsrate) {
+        List<String> savingsResults = application.calculateSavings(savingsrate);
+        StringBuilder response = new StringBuilder("Das eingegebene Gehalt ist: " + salary + "\nErsparnisse:\n");
+        for (String result : savingsResults) {
+            response.append(result).append("\n");
+        }
+
+        return response.toString();
+    }
+}
--- src/main/java/org/example/money/MoneyApplication.java
+++ src/main/java/org/example/money/MoneyApplication.java
@@ -2,9 +2,12 @@ package org.example.money;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.util.ArrayList;
+import java.util.List;
 @SpringBootApplication
 public class MoneyApplication {
+    public int salary;
+
     public static void main(String[] args) {
         SpringApplication.run(MoneyApplication.class, args);
         {
@@ -13,4 +16,16 @@ public class MoneyApplication {
 
     }
 
+    public List<String> calculateSavings(int savingsRate) {
+        int years = 12;
+        int totalSavings = 0;
+        List<String> results = new ArrayList<>();
+
+        for (int year = 1; year <= years; year++) {
+            totalSavings += savingsRate*12;
+            results.add("Jahr " + year + ": " + totalSavings);
+        }
+
+        return results;
+    }
 }
