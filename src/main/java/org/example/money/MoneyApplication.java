de4d2ac0202c875f45f5b2d300bc56943425eb28
nabil.nabil <nabil.nabil@otto.de>
2024-11-15 10:25:10 +0100

    ALBA-0001| Nabil | Connection to database done  also user can add or delete his category.

--- src/main/java/org/example/money/MoneyApplication.java
+++ src/main/java/org/example/money/MoneyApplication.java
@@ -15,9 +15,8 @@ public class MoneyApplication {
         SpringApplication.run(MoneyApplication.class, args);
         {
             System.out.println("Hello");
-
+            MoneyApplication app = new MoneyApplication();
         }
-
     }
 
     public List<String> calculateSavings(int savingsRate) {
@@ -31,6 +30,37 @@ public class MoneyApplication {
         }
 
         return results;
+    }
+    public void AddBudget(int ID,String category, int budget)
+    {
+        Connection connection = null;
+
+        try {
+            connection = new Connectdb().getConnection();
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO BUDGET (ID, Category, Budget) VALUES (?,?,?)");
+            statement.setInt(1, ID);
+            statement.setString(2, category);
+            statement.setInt(3, budget);
+            statement.executeUpdate();
+
+
+        }catch (Exception e) {
+
+            e.printStackTrace();}
+    }
+
+    public void deleteFromBudget(int ID, String category){
+        Connection connection = null;
+
+        try{
+            connection = new Connectdb().getConnection();
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM BUDGET WHERE ID = ? AND Category = ?");
+            statement.setInt(1, ID);
+            statement.setString(2, category);
+            statement.executeUpdate();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
