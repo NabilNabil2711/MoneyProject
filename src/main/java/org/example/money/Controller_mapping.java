c3154748cc66e14ddacd18857077add595031d9c
NabilNabil2711 <nabil.nabil@otto.de>
2024-11-15 12:59:44 +0100

    Feature alba 0002 insert to database (#3)
    * ALBA-0001| Nabil | Connection to database done  also user can add or delete his category.
    
    * ALBA-0001| Nabil | Do some frontend to make it look better

--- src/main/java/org/example/money/Controller_mapping.java
+++ src/main/java/org/example/money/Controller_mapping.java
@@ -1,11 +1,9 @@
 package org.example.money;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import java.util.List;
+
 
 @Controller
 public class Controller_mapping {
@@ -18,26 +16,20 @@ public class Controller_mapping {
     public String hello() {
         return "index";
     }
-    @GetMapping("calculate")
-    public String calculate() {
-        return "Calculator";
-    }
-    @GetMapping("addKathegorie")
-    public String addKathegorie()
-    {
-        return "BudgetForm";
-    }
+
 
     //Get input from User
     //-------------------------
-    @PostMapping("/getSalary")
-    @ResponseBody
-    public String getSalary(@RequestParam("salary")int salary,@RequestParam("savingsrate")int savingsrate) {
-        List<String> savingsResults = application.calculateSavings(savingsrate);
-        StringBuilder response = new StringBuilder("Das eingegebene Gehalt ist: " + salary + "\nErsparnisse:\n");
-        for (String result : savingsResults) {
-            response.append(result).append("\n");
-        }
-        return response.toString();
+
+    @PostMapping("/getBudget")
+    public String getBudget(@RequestParam("budget_name")int budget,@RequestParam("Category") String Category)
+    {
+        application.AddBudget(1,Category,budget);
+        return "index";
+    }
+    @PostMapping("/deleteBudget")
+    public String deleteBudget(@RequestParam("Category")String Category){
+        application.deleteFromBudget(1,Category);
+        return "index";
     }
 }
